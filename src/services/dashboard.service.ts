5e9ecd54e7bff2ad2c4b9e9c97bb1faaff375a11
Chathura Prasanga <chathuraprasanga98@gmail.com>
2024-12-30 00:10:06 +0530

    today cheques

--- src/services/dashboard.service.ts
+++ src/services/dashboard.service.ts
@@ -2,11 +2,15 @@ import { countUsers, findUserRepo } from "../repositories/user.repository";
 import { countCustomers } from "../repositories/customer.repository";
 import { countProducts } from "../repositories/product.repository";
 import { countSuppliers } from "../repositories/supplier.repository";
-import { countCheques } from "../repositories/cheque.repository";
+import {
+    countCheques,
+    findChequesRepo,
+} from "../repositories/cheque.repository";
 import { countInvoices } from "../repositories/invoice.repository";
 
 export const findDashboardDetailsService = async (data: any) => {
     try {
+        const { chequesCount, totalAmount } = await getChequesToDeposit();
         return {
             user: await findUserRepo({ _id: data.userId }),
             usersCount: await countUsers({}),
@@ -15,7 +19,35 @@ export const findDashboardDetailsService = async (data: any) => {
             suppliersCount: await countSuppliers({}),
             chequesCount: await countCheques({}),
             invoicesCount: await countInvoices({}),
+            chequesToDeposit: {
+                count: chequesCount,
+                amount: totalAmount,
+            },
         };
+    } catch (e: any) {
+        console.error(e.message);
+        throw e;
+    }
+};
+
+const getChequesToDeposit = async () => {
+    try {
+        const date = new Date();
+        const todayUtc = new Date(
+            Date.UTC(
+                date.getUTCFullYear(),
+                date.getUTCMonth(),
+                date.getUTCDate()
+            )
+        );
+        const today = todayUtc.toISOString();
+        const cheques = await findChequesRepo({ depositDate: today });
+        const chequesCount = cheques.length;
+        const totalAmount = cheques.reduce(
+            (sum, cheque) => sum + cheque.amount,
+            0
+        );
+        return { chequesCount, totalAmount };
     } catch (e: any) {
         console.error(e.message);
         throw e;
