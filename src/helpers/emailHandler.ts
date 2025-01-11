a26ad8ce27cd6e2d664f9beb3917c252072ffb3e
Chathura Prasanga <chathuraprasanga98@gmail.com>
2025-01-11 22:55:00 +0530

    changes

--- src/helpers/emailHandler.ts
+++ src/helpers/emailHandler.ts
@@ -1,3 +1,5 @@
+import { rolePreview } from "./preview";
+
 export const EMAIL_TYPES = {
     ADD_USER: "ADD_USER",
     FORGOT_PASSWORD: "FORGOT_PASSWORD",
@@ -34,10 +36,10 @@ export const createEmailBody = async (type: string, data: any) => {
                     <p>Here is your login details</p>
                     <p>Email: ${data.email}</p>
                     <p>Phone: ${data.phone}</p>
-                    <p>Role: ${data.role}</p>
+                    <p>Role: ${rolePreview(data.role)}</p>
                     <p>Password: ${data.password}</p>
                     <br>
-                    <p>Please change your password immediatly. Click here to <a href="https://www.wd-app.xcorpion.xyz/login">login</a></p>
+                    <p>Please change your password immediatly. Click here to <a href="https://wd-app.xcorpion.xyz/login">login</a></p>
                     <br>
                     <p>If you have any questions, feel free to reach out to our support team at <a href="mailto:support@xcorpion.xyz">support@xcorpion.xyz</a>.</p>
                     <p>Thank you for joining us!</p>
